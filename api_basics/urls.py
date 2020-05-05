9bb4cd43026ba3c52746d2c951e3deebc4d24e11
root <root@localhost.localdomain>
2020-05-04 23:10:37 -0400

    with loginPage

--- api_basics/urls.py
+++ api_basics/urls.py
@@ -3,6 +3,7 @@ from api_basics import views
 from .views import ArticleList,ArticleDetail,ArticleViewSet
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
+from django.contrib.auth.views import LoginView,LogoutView
 
 router = DefaultRouter()
 router.register('article', ArticleViewSet, basename='article')
@@ -17,6 +18,8 @@ urlpatterns = [
     path('viewset/<int:pk>/',include(router.urls)),
     path('',include('djoser.urls')),
     path('',include('djoser.urls.authtoken')),
+    path('login/',LoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(),name='logout'),
 
 ]
 
